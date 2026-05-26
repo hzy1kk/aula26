2f185cef3866f05a2d8b0e9f6ab9191ab077d1c3
hzy1kk <lucaslohan@colegiopaulodetarso.com.br>
2026-05-26 14:50:16 +0000

    feito1

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
-
 def index():
-    return "hello"
+    return render_template("index.html")
+
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
